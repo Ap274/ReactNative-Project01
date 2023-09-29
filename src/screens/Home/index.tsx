d3801c1633978b7ade055cc9157fbefb70ee7807
Ap274 <aldopiresaps@gmail.com>
2023-09-29 11:42:18 -0300

    feat: organizing the project into folders

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do Evento
+      </Text>
+      
+      <Text style={styles.eventDate}>
+        Sexta, 29 de setembro de 2023
+      </Text>
+    </View>
+  )
+}
