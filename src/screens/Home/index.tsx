0fa14c001ad509a6da1042747d0fd0b20b5ed34f
Ap274 <aldopiresaps@gmail.com>
2023-09-29 15:00:17 -0300

    feat: creating Form basic structure

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,11 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
-export default function Home() {
+export function Home() {
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de Adicionar!");
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>
@@ -11,6 +15,20 @@ export default function Home() {
       <Text style={styles.eventDate}>
         Sexta, 29 de setembro de 2023
       </Text>
+
+      <View style={styles.form}>
+        <TextInput 
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
