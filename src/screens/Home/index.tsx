92ba774020789fcde388bc676948fb9c5929b60a
Ap274 <aldopiresaps@gmail.com>
2023-10-02 14:42:42 -0300

    feat: adding and removing participants using hooks

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,26 +1,28 @@
-import { Text, View, TextInput, TouchableOpacity, FlatList, Alert } from "react-native";
+import { Text, View, TextInput, TouchableOpacity, FlatList, Alert, Pressable } from "react-native";
+import React, { useState } from "react";
 
 import { Participant } from "../../components/Participant";
 
 import { styles } from "./styles";
 
 export function Home() {
-
-  const participants = [
-    'Rodrigo', 'Vini', 'Diego', 'Biro', 'Ana', 'Isa', 'Jack', 'Mayk', 'John', 'Devon', 'Ermes'
-  ];
+  const [participants, setParticipants] = useState<string[]>([]);
+  const [participantName, setParticipantName] = useState('');
 
   function handleParticipantAdd() {
-    if(participants.includes('Rodrigo')) {
+    if(participants.includes(participantName)) {
       return Alert.alert('Participante Existe', 'Já existe um participante na lista com esse nome')
     }
+
+    setParticipants(prevState => [...prevState, participantName]);
+    setParticipantName('');  
   }
 
   function handleParticipantRemove(name: string) {
     Alert.alert('Remover', `Remover o participante ${name}?`, [
       {
         text: 'Sim',
-        onPress: () => Alert.alert('Deletado!')
+        onPress: () => setParticipants(prevState => prevState.filter(participant => participant !== name))
       },
       {
         text: 'Não',
@@ -44,13 +46,15 @@ export function Home() {
           style={styles.input}
           placeholder="Nome do participante"
           placeholderTextColor="#6B6B6B"
+          onChangeText={setParticipantName}
+          value={participantName}
         />
 
-        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Pressable style={styles.button} onPress={handleParticipantAdd}>
           <Text style={styles.buttonText}>
             +
           </Text>
-        </TouchableOpacity>
+        </Pressable>
       </View>
 
       <FlatList 
